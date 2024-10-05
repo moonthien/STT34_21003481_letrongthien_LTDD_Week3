475462ed80d985451ed425ec8939bfcf60fa8a48
moonthien <letrongthien23032003@gmail.com>
2024-10-05 17:47:03 +0700

    xong week 3

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import XMEye from './screens/xmeye';
+import React from 'react';
+import FirstScreen from './screens/firstScreen';
+import Cau2a from './screens/cau2a';
+import Cau1a from './screens/cau1a';
+import Cau1b from './screens/cau1b';
+import Cau1c from './screens/cau1c';
+import Cau1d from './screens/cau1d';
+import Cau1e from './screens/cau1e';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <FirstScreen />
+    //<Cau1a />
+    // <Cau1b />
+     //<Cau1c />
+    // <Cau1d />
+    // <Cau1e />
+    // <Cau2a />
+    // <XMEye />
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
